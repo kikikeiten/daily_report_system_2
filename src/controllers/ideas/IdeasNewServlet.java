1bc8ec2bc962ac07ed6f9aa9cfed1776a221c925
keiten.kiki <kikikeiten@gmail.com>
2020-11-17 10:45:32 +0900

    :recycle: #38 Fix IdeasNewServlet.java

--- src/controllers/ideas/IdeasNewServlet.java
+++ src/controllers/ideas/IdeasNewServlet.java
@@ -12,33 +12,26 @@ import javax.servlet.http.HttpServletResponse;
 
 import models.Idea;
 
-/**
- * Servlet implementation class ReportsNewServlet
- */
-@WebServlet("/reports/new")
-public class ReportsNewServlet extends HttpServlet {
+@WebServlet("/ideas/new")
+public class IdeasNewServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ReportsNewServlet() {
+    public IdeasNewServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
         request.setAttribute("_token", request.getSession().getId());
 
-        Idea r = new Idea();
-        r.setReport_date(new Date(System.currentTimeMillis()));
-        request.setAttribute("report", r);
+        Idea i = new Idea();
 
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/reports/new.jsp");
+        i.setCreated_date(new Date(System.currentTimeMillis()));
+
+        request.setAttribute("idea", i);
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/ideas/new.jsp");
         rd.forward(request, response);
     }
 
