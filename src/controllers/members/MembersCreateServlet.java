26bdbe57fb639b87827d97ae46bba4d83b3d45a4
keiten.kiki <kikikeiten@gmail.com>
2020-11-17 12:27:09 +0900

    :recycle: #38 Fix MembersCreateServlet.java

--- src/controllers/members/MembersCreateServlet.java
+++ src/controllers/members/MembersCreateServlet.java
@@ -17,63 +17,57 @@ import models.validators.MemberValidator;
 import utils.DBUtil;
 import utils.EncryptUtil;
 
-/**
- * Servlet implementation class EmployeesCreateServlet
- */
-@WebServlet("/employees/create")
-public class EmployeesCreateServlet extends HttpServlet {
+@WebServlet("/members/create")
+public class MembersCreateServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public EmployeesCreateServlet() {
+    public MembersCreateServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-     */
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
         String _token = (String) request.getParameter("_token");
+
         if (_token != null && _token.equals(request.getSession().getId())) {
+
             EntityManager em = DBUtil.createEntityManager();
 
-            Member e = new Member();
+            Member m = new Member();
+            Timestamp currentTime = new Timestamp(System.currentTimeMillis());
 
-            e.setCode(request.getParameter("code"));
-            e.setName(request.getParameter("name"));
-            e.setPassword(
+            m.setCode(request.getParameter("code"));
+            m.setName(request.getParameter("name"));
+            m.setPassword(
                     EncryptUtil.getPasswordEncrypt(
                             request.getParameter("password"),
                             (String) this.getServletContext().getAttribute("pepper")));
-            e.setAdmin_flag(Integer.parseInt(request.getParameter("admin_flag")));
+            m.setRole_flag(Integer.parseInt(request.getParameter("role_flag")));
+            m.setDelete_flag(0);
+            m.setCreated_at(currentTime);
+            m.setUpdated_at(currentTime);
 
-            Timestamp currentTime = new Timestamp(System.currentTimeMillis());
-            e.setCreated_at(currentTime);
-            e.setUpdated_at(currentTime);
-            e.setDelete_flag(0);
+            List<String> errors = MemberValidator.validate(m, true, true);
 
-            List<String> errors = MemberValidator.validate(e, true, true);
             if (errors.size() > 0) {
                 em.close();
 
                 request.setAttribute("_token", request.getSession().getId());
-                request.setAttribute("employee", e);
+                request.setAttribute("member", m);
                 request.setAttribute("errors", errors);
 
-                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/employees/new.jsp");
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/members/new.jsp");
                 rd.forward(request, response);
+
             } else {
                 em.getTransaction().begin();
-                em.persist(e);
+                em.persist(m);
                 em.getTransaction().commit();
                 em.close();
-                request.getSession().setAttribute("flush", "登録が完了しました。");
+                request.getSession().setAttribute("toast", "登録が完了しました。");
 
-                response.sendRedirect(request.getContextPath() + "/employees");
+                response.sendRedirect(request.getContextPath() + "/members");
             }
         }
     }
