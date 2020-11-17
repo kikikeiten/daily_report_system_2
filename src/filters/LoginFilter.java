a119ca844aaf857369d44a0411bb0f2a8237a8de
keiten.kiki <kikikeiten@gmail.com>
2020-11-17 09:21:13 +0900

    :recycle: #38 Fix LoginFilter.java

--- src/filters/LoginFilter.java
+++ src/filters/LoginFilter.java
@@ -15,29 +15,15 @@ import javax.servlet.http.HttpSession;
 
 import models.Member;
 
-/**
- * Servlet Filter implementation class LoginFilter
- */
 @WebFilter("/*")
 public class LoginFilter implements Filter {
 
-    /**
-     * Default constructor.
-     */
     public LoginFilter() {
-        // TODO Auto-generated constructor stub
     }
 
-    /**
-     * @see Filter#destroy()
-     */
     public void destroy() {
-        // TODO Auto-generated method stub
     }
 
-    /**
-     * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
-     */
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
         String context_path = ((HttpServletRequest) request).getContextPath();
@@ -47,25 +33,21 @@ public class LoginFilter implements Filter {
             HttpSession session = ((HttpServletRequest) request).getSession();
 
             // セッションスコープに保存された従業員（ログインユーザ）情報を取得
-            Member e = (Member) session.getAttribute("login_employee");
+            Member m = (Member) session.getAttribute("login_member");
 
             if (!servlet_path.equals("/login")) { // ログイン画面以外について
                 // ログアウトしている状態であれば
                 // ログイン画面にリダイレクト
-                if (e == null) {
+                if (m == null) {
                     ((HttpServletResponse) response).sendRedirect(context_path + "/login");
                     return;
                 }
 
-                // 従業員管理の機能は管理者のみが閲覧できるようにする
-                if (servlet_path.matches("/employees.*") && e.getAdmin_flag() == 0) {
-                    ((HttpServletResponse) response).sendRedirect(context_path + "/");
-                    return;
-                }
-            } else { // ログイン画面について
+            } else {
+                // ログイン画面について
                 // ログインしているのにログイン画面を表示させようとした場合は
                 // システムのトップページにリダイレクト
-                if (e != null) {
+                if (m != null) {
                     ((HttpServletResponse) response).sendRedirect(context_path + "/");
                     return;
                 }
@@ -75,11 +57,6 @@ public class LoginFilter implements Filter {
         chain.doFilter(request, response);
     }
 
-    /**
-     * @see Filter#init(FilterConfig)
-     */
     public void init(FilterConfig fConfig) throws ServletException {
-        // TODO Auto-generated method stub
     }
-
 }
