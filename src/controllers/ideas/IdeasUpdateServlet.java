24b1ca3cc3ad29d30da78cd20ec08817b606965d
keiten.kiki <kikikeiten@gmail.com>
2020-11-17 12:02:25 +0900

    :recycle: #38 Fix  IdeasUpdateServlet.java

--- src/controllers/ideas/IdeasUpdateServlet.java
+++ src/controllers/ideas/IdeasUpdateServlet.java
@@ -18,105 +18,102 @@ import models.Idea;
 import models.validators.IdeaValidator;
 import utils.DBUtil;
 
-/**
- * Servlet implementation class ReportsUpdateServlet
- */
-@WebServlet("/reports/update")
-public class ReportsUpdateServlet extends HttpServlet {
+@WebServlet("/ideas/update")
+public class IdeasUpdateServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ReportsUpdateServlet() {
+    public IdeasUpdateServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-     */
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
         String _token = (String) request.getParameter("_token");
+
         if (_token != null && _token.equals(request.getSession().getId())) {
+
             EntityManager em = DBUtil.createEntityManager();
 
-            Idea r = em.find(Idea.class, (Integer) (request.getSession().getAttribute("report_id")));
-            Integer submit = Integer.parseInt(request.getParameter("submit"));
-            Member e = (Member) request.getSession().getAttribute("login_employee");
+            Idea i = em.find(Idea.class, (Integer) (request.getSession().getAttribute("idea_id")));
 
-            r.setReport_date(Date.valueOf(request.getParameter("report_date")));
-            r.setTitle(request.getParameter("title"));
-            r.setContent(request.getParameter("content"));
-            r.setUpdated_at(new Timestamp(System.currentTimeMillis()));
-            r.setApproval(submit);
+            Integer review_flag_int = Integer.parseInt(request.getParameter("review_flag"));
 
-            List<String> errors = IdeaValidator.validate(r);
+            Member login_member = (Member) request.getSession().getAttribute("login_member");
+
+            i.setTitle(request.getParameter("title"));
+            i.setContent(request.getParameter("content"));
+            i.setReview_flag(review_flag_int);
+            i.setCreared_date(Date.valueOf(request.getParameter("created_date")));
+            i.setUpdated_at(new Timestamp(System.currentTimeMillis()));
+
+            List<String> errors = IdeaValidator.validate(i);
+
             if (errors.size() > 0) {
                 em.close();
 
                 request.setAttribute("_token", request.getSession().getId());
-                request.setAttribute("report", r);
+                request.setAttribute("idea", i);
                 request.setAttribute("errors", errors);
 
-                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/reports/edit.jsp");
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/ideas/edit.jsp");
                 rd.forward(request, response);
+
             } else {
                 em.getTransaction().begin();
                 em.getTransaction().commit();
                 em.close();
 
-                if (e.getAdmin_flag() == 0 || e.getAdmin_flag() == 1) {
-                    switch (submit) {
+                if (login_member.getRole_flag() == 0 || login_member.getRole_flag() == 1) {
+                    switch (review_flag_int) {
                     case 0:
-                        request.getSession().setAttribute("flush",
+                        request.getSession().setAttribute("toast",
                                 "日報「" + request.getParameter("title") + "」を下書きとして保存しました。");
                         break;
                     case 2:
-                        request.getSession().setAttribute("flush",
+                        request.getSession().setAttribute("toast",
                                 "日報「" + request.getParameter("title") + "」を課長に提出しました。");
                         break;
                     case 4:
-                        request.getSession().setAttribute("flush",
+                        request.getSession().setAttribute("toast",
                                 "日報「" + request.getParameter("title") + "」を部長に再提出しました。");
                         break;
                     }
                 }
 
-                if (e.getAdmin_flag() == 2) {
-                    switch (submit) {
+                if (login_member.getRole_flag() == 2) {
+                    switch (review_flag_int) {
                     case 0:
-                        request.getSession().setAttribute("flush",
+                        request.getSession().setAttribute("toast",
                                 "日報「" + request.getParameter("title") + "」を下書きとして保存しました。");
                         break;
                     case 2:
-                        request.getSession().setAttribute("flush",
+                        request.getSession().setAttribute("toast",
                                 "日報「" + request.getParameter("title") + "」を他課長に提出しました。");
                         break;
                     case 4:
-                        request.getSession().setAttribute("flush",
+                        request.getSession().setAttribute("toast",
                                 "日報「" + request.getParameter("title") + "」を部長に再提出しました。");
                         break;
                     }
                 }
 
-                if (e.getAdmin_flag() == 3) {
-                    switch (submit) {
+                if (login_member.getRole_flag() == 3) {
+                    switch (review_flag_int) {
                     case 0:
-                        request.getSession().setAttribute("flush",
+                        request.getSession().setAttribute("toast",
                                 "日報「" + request.getParameter("title") + "」を下書きとして保存しました。");
                         break;
                     case 4:
-                        request.getSession().setAttribute("flush",
+                        request.getSession().setAttribute("toast",
                                 "日報「" + request.getParameter("title") + "」を他部長に提出しました。");
                         break;
                     }
                 }
 
-                request.getSession().removeAttribute("report_id");
+                request.getSession().removeAttribute("idea_id");
 
-                response.sendRedirect(request.getContextPath() + "/reports");
+                response.sendRedirect(request.getContextPath() + "/ideas");
             }
         }
     }
