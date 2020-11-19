1fc2abecd5cb2ae1dda262087cadc8455a8aef42
keiten.kiki <kikikeiten@gmail.com>
2020-11-19 23:13:22 +0900

    :fire: #38 Remove unnecessary comments

--- src/controllers/members/MembersShowServlet.java
+++ src/controllers/members/MembersShowServlet.java
@@ -13,24 +13,14 @@ import javax.servlet.http.HttpServletResponse;
 import models.Member;
 import utils.DBUtil;
 
-/**
- * Servlet implementation class EmployeesShowServlet
- */
 @WebServlet("/employees/show")
 public class MembersShowServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public MembersShowServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
