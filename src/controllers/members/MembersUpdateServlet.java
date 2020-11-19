1e12959751dc80474ac692663ffa1e7cb021d90a
keiten.kiki <kikikeiten@gmail.com>
2020-11-19 23:02:41 +0900

    :recycle: #38 Fix MembersUpdateServlet

--- src/controllers/members/MembersUpdateServlet.java
+++ src/controllers/members/MembersUpdateServlet.java
@@ -17,7 +17,7 @@ import models.validators.MemberValidator;
 import utils.DBUtil;
 import utils.EncryptUtil;
 
-@WebServlet("/employees/update")
+@WebServlet("/members/update")
 public class MembersUpdateServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
@@ -27,58 +27,65 @@ public class MembersUpdateServlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
         String _token = (String) request.getParameter("_token");
+
         if (_token != null && _token.equals(request.getSession().getId())) {
             EntityManager em = DBUtil.createEntityManager();
 
-            Member e = em.find(Member.class, (Integer) (request.getSession().getAttribute("employee_id")));
+            Member m = em.find(Member.class, (Integer) (request.getSession().getAttribute("member_id")));
 
-            // 現在の値と異なる社員番号が入力されていたら
+            // 現在の値と異なるmember_idが入力されていたら
             // 重複チェックを行う指定をする
             Boolean code_duplicate_check = true;
-            if (e.getCode().equals(request.getParameter("code"))) {
+
+            if (m.getCode().equals(request.getParameter("code"))) {
                 code_duplicate_check = false;
             } else {
-                e.setCode(request.getParameter("code"));
+                m.setCode(request.getParameter("code"));
             }
 
             // パスワード欄に入力があったら
             // パスワードの入力値チェックを行う指定をする
             Boolean password_check_flag = true;
+
             String password = request.getParameter("password");
+
             if (password == null || password.equals("")) {
                 password_check_flag = false;
             } else {
-                e.setPassword(
+                m.setPassword(
                         EncryptUtil.getPasswordEncrypt(
                                 password,
                                 (String) this.getServletContext().getAttribute("pepper")));
             }
 
-            e.setName(request.getParameter("name"));
-            e.setAdmin_flag(Integer.parseInt(request.getParameter("admin_flag")));
-            e.setUpdated_at(new Timestamp(System.currentTimeMillis()));
-            e.setDelete_flag(0);
+            m.setName(request.getParameter("name"));
+            m.setRole_flag(Integer.parseInt(request.getParameter("role_flag")));
+            m.setDelete_flag(0);
+            m.setUpdated_at(new Timestamp(System.currentTimeMillis()));
 
-            List<String> errors = MemberValidator.validate(e, code_duplicate_check, password_check_flag);
+            List<String> errors = MemberValidator.validate(m, code_duplicate_check, password_check_flag);
+
             if (errors.size() > 0) {
                 em.close();
 
                 request.setAttribute("_token", request.getSession().getId());
-                request.setAttribute("employee", e);
+                request.setAttribute("member", m);
                 request.setAttribute("errors", errors);
 
-                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/employees/edit.jsp");
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/members/edit.jsp");
                 rd.forward(request, response);
+
             } else {
                 em.getTransaction().begin();
                 em.getTransaction().commit();
                 em.close();
-                request.getSession().setAttribute("flush", "更新が完了しました。");
+                request.getSession().setAttribute("toast", "Update completed!");
 
-                request.getSession().removeAttribute("employee_id");
+                request.getSession().removeAttribute("member_id");
 
-                response.sendRedirect(request.getContextPath() + "/employees");
+                response.sendRedirect(request.getContextPath() + "/members");
             }
         }
     }
