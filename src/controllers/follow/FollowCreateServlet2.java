9e1bde34067e423a3c3c007006e14ae25be3c879
keiten.kiki <kikikeiten@gmail.com>
2020-11-20 19:01:28 +0900

    :fire: #37 Delete unnecessary comments

--- src/controllers/follow/FollowCreateServlet2.java
+++ src/controllers/follow/FollowCreateServlet2.java
@@ -14,27 +14,16 @@ import models.Member;
 import models.Follow;
 import utils.DBUtil;
 
-/**
- * Servlet implementation class FollowCreateServlet2
- */
 @WebServlet("/follow/create/2")
 public class FollowCreateServlet2 extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public FollowCreateServlet2() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-     */
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        // TODO Auto-generated method stub
 
         EntityManager em = DBUtil.createEntityManager();
 
