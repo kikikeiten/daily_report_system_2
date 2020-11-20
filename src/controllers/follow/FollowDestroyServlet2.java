00456cdd5553c693d135564108a40fb57c9b8271
keiten.kiki <kikikeiten@gmail.com>
2020-11-20 19:53:58 +0900

    :recycle: #38 Fix FollowDestroyServlet2

--- src/controllers/follow/FollowDestroyServlet2.java
+++ src/controllers/follow/FollowDestroyServlet2.java
@@ -26,25 +26,26 @@ public class FollowDestroyServlet2 extends HttpServlet {
 
         EntityManager em = DBUtil.createEntityManager();
 
-        Member login_employee = (Member) request.getSession().getAttribute("login_employee");
-        Member e = em.find(Member.class, Integer.parseInt(request.getParameter("employee_id")));
+        Member login_member = (Member) request.getSession().getAttribute("login_member");
+        Member member = em.find(Member.class, Integer.parseInt(request.getParameter("member_id")));
 
         Integer ei = 0;
-        ei = em.createNamedQuery("followDestroy", Integer.class)
-                .setParameter("follow", e)
-                .setParameter("employee", login_employee)
+        ei = em.createNamedQuery("getDestroyFollow", Integer.class)
+                .setParameter("followed_id", member)
+                .setParameter("login_member", login_member)
                 .getSingleResult();
 
         Follow f = em.find(Follow.class, ei);
 
-        String unfollow_name = e.getName();
+        String unfollow_name_str = member.getName();
 
         em.getTransaction().begin();
         em.remove(f);
         em.getTransaction().commit();
         em.close();
-        request.getSession().setAttribute("flush", unfollow_name + "さんのフォローを解除しました。");
 
-        response.sendRedirect(request.getContextPath() + "/employees");
+        request.getSession().setAttribute("flush", unfollow_name_str + "さんのフォローを解除しました。");
+
+        response.sendRedirect(request.getContextPath() + "/members");
     }
 }
