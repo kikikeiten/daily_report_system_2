5f303898026e26acdb3ffb555fc59bdb47f419cd
keiten.kiki <kikikeiten@gmail.com>
2020-11-20 18:50:50 +0900

    :recycle: #37 Delete unnecessary comments

--- src/controllers/follow/FollowCreateServlet.java
+++ src/controllers/follow/FollowCreateServlet.java
@@ -15,24 +15,14 @@ import models.Follow;
 import models.Idea;
 import utils.DBUtil;
 
-/**
- * Servlet implementation class FollowCreateServlet
- */
 @WebServlet("/follow/create")
 public class FollowCreateServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public FollowCreateServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-     */
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         // TODO Auto-generated method stub
