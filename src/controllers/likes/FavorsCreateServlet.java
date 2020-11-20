f5b7ea2b45578859fce90172fd24d70e09390465
keiten.kiki <kikikeiten@gmail.com>
2020-11-20 18:47:55 +0900

    :bulb: #37 Add comments

--- src/controllers/likes/FavorsCreateServlet.java
+++ src/controllers/likes/FavorsCreateServlet.java
@@ -32,24 +32,31 @@ public class FavorsCreateServlet extends HttpServlet {
 
         Favor f = new Favor();
 
+        // 詳細な現在時刻を取得
         Timestamp currentTime = new Timestamp(System.currentTimeMillis());
 
+        // Favorテーブルにセット
         f.setMember((Member) request.getSession().getAttribute("login_member"));
         f.setIdea(i);
         f.setCreated_at(currentTime);
         f.setUpdated_at(currentTime);
 
+        // Ideaテーブルにセット
         i.setFavors(Integer.parseInt(request.getParameter("favors")) + i.getFavors());
 
+        // favorされたメンバーの氏名を取得
         Member member = i.getMember();
         String member_name_str = member.getName();
 
+        // favorされたideaのタイトルを取得
         String idea_title_str = i.getTitle();
 
         em.getTransaction().begin();
         em.persist(f);
         em.getTransaction().commit();
         em.close();
+
+        // トーストメッセージ
         request.getSession().setAttribute("toast", member_name_str + "さんの日報「" + idea_title_str + "」にいいねしました。");
 
         response.sendRedirect(request.getContextPath() + "/ideas");
