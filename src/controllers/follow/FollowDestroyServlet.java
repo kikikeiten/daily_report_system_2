c5425abe47dc55f3a8def057d62e6e5894934452
keiten.kiki <kikikeiten@gmail.com>
2020-11-20 19:12:14 +0900

    :bulb: #37 Add comments

--- src/controllers/follow/FollowDestroyServlet.java
+++ src/controllers/follow/FollowDestroyServlet.java
@@ -29,8 +29,10 @@ public class FollowDestroyServlet extends HttpServlet {
 
         Idea idea = em.find(Idea.class, Integer.parseInt(request.getParameter("member_id")));
 
+        // ログイン中のメンバーIDを取得
         Member login_member = (Member) request.getSession().getAttribute("login_member");
 
+        // フォロー解除するメンバーIDを取得
         Integer ei = 0;
         ei = em.createNamedQuery("getDestroyFollow", Integer.class)
                 .setParameter("followed_id", idea.getMember())
@@ -39,6 +41,7 @@ public class FollowDestroyServlet extends HttpServlet {
 
         Follow f = em.find(Follow.class, ei);
 
+        // フォロー解除するメンバーの氏名を取得
         Member unfollow = idea.getMember();
         String unfollow_name_str = unfollow.getName();
 
@@ -47,6 +50,7 @@ public class FollowDestroyServlet extends HttpServlet {
         em.getTransaction().commit();
         em.close();
 
+        // トーストメッセージ
         request.getSession().setAttribute("toast", unfollow_name_str + "さんのフォローを解除しました。");
 
         response.sendRedirect(request.getContextPath() + "/ideas");
