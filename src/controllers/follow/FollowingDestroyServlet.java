108af9c6c0855995afea3032d6fcfcb34f62bfc2
keiten.kiki <kikikeiten@gmail.com>
2020-11-20 20:41:38 +0900

    :fire: #37 Delete unnecessary comments

--- src/controllers/follow/FollowingDestroyServlet.java
+++ src/controllers/follow/FollowingDestroyServlet.java
@@ -13,27 +13,16 @@ import models.Member;
 import models.Follow;
 import utils.DBUtil;
 
-/**
- * Servlet implementation class FollowingDestroyServlet
- */
 @WebServlet("/following/destroy")
 public class FollowingDestroyServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public FollowingDestroyServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-     */
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        // TODO Auto-generated method stub
 
         EntityManager em = DBUtil.createEntityManager();
         Follow f = em.find(Follow.class, Integer.parseInt(request.getParameter("follow_id")));
