a7bb2c3144e92db6ecf4489c0f8d05b5f093e792
keiten.kiki <kikikeiten@gmail.com>
2020-11-21 20:13:21 +0900

    :fire: #37 Delete unnecessary comments

--- src/controllers/follow/TimelineIndexServlet.java
+++ src/controllers/follow/TimelineIndexServlet.java
@@ -16,27 +16,16 @@ import models.Member;
 import models.Idea;
 import utils.DBUtil;
 
-/**
- * Servlet implementation class TimelineIndexServlet
- */
 @WebServlet("/timeline")
 public class TimelineIndexServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public TimelineIndexServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        // TODO Auto-generated method stub
 
         EntityManager em = DBUtil.createEntityManager();
 
