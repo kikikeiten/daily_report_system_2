bf27dac558e322da676ce862c601ab89766b3ae3
keiten.kiki <kikikeiten@gmail.com>
2020-11-21 20:09:36 +0900

    :fire: #37 Delete unnecessary comments

--- src/controllers/follow/FollowingIndexServlet.java
+++ src/controllers/follow/FollowingIndexServlet.java
@@ -15,27 +15,16 @@ import models.Member;
 import models.Follow;
 import utils.DBUtil;
 
-/**
- * Servlet implementation class FollowingIndexServlet
- */
 @WebServlet("/following")
 public class FollowingIndexServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public FollowingIndexServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        // TODO Auto-generated method stub
 
         EntityManager em = DBUtil.createEntityManager();
 
