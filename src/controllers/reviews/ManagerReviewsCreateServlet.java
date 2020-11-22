0d84c178e825864ad56611b59eca7a15fa77ed29
keiten.kiki <kikikeiten@gmail.com>
2020-11-22 20:52:05 +0900

    :fire: #37 Delete unnecessary comments

--- src/controllers/reviews/ManagerReviewsCreateServlet.java
+++ src/controllers/reviews/ManagerReviewsCreateServlet.java
@@ -15,27 +15,16 @@ import models.Member;
 import models.Idea;
 import utils.DBUtil;
 
-/**
- * Servlet implementation class ManagerApprovalCreate
- */
 @WebServlet("/manager/approval/create")
 public class ManagerReviewsCreateServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public ManagerReviewsCreateServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-     */
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        // TODO Auto-generated method stub
 
         EntityManager em = DBUtil.createEntityManager();
 
