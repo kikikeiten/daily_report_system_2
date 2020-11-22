0f5ebf7538844955d0fcb450a848a6c9e701d7b3
keiten.kiki <kikikeiten@gmail.com>
2020-11-22 21:28:16 +0900

    :recycle: #38 Fix ManagerAdviceCreateServlet

--- src/controllers/reviews/ManagerAdviceCreateServlet.java
+++ src/controllers/reviews/ManagerAdviceCreateServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import models.Idea;
 import utils.DBUtil;
 
-@WebServlet("/manager/remand/create")
+@WebServlet("/advice/manager/create")
 public class ManagerAdviceCreateServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
@@ -25,17 +25,17 @@ public class ManagerAdviceCreateServlet extends HttpServlet {
 
         EntityManager em = DBUtil.createEntityManager();
 
-        Idea r = em.find(Idea.class, Integer.parseInt(request.getParameter("report_id")));
+        Idea idea = em.find(Idea.class, Integer.parseInt(request.getParameter("ideaId")));
 
-        r.setApproval(Integer.parseInt(request.getParameter("submit")));
+        idea.setReview_flag(Integer.parseInt(request.getParameter("reviewFlag")));
 
         em.getTransaction().begin();
         em.getTransaction().commit();
         em.close();
 
-        request.getSession().setAttribute("flush", "日報「" + r.getTitle() + "」を課長に再提出しました。");
+        request.getSession().setAttribute("toast", "日報「" + idea.getTitle() + "」を課長に再提出しました。");
 
-        response.sendRedirect(request.getContextPath() + "/remand/manager");
+        response.sendRedirect(request.getContextPath() + "/advice/manager");
     }
 
 }
