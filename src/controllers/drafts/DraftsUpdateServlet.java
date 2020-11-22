6d0e43fcc98420ee8db8848eed648550bce78b96
keiten.kiki <kikikeiten@gmail.com>
2020-11-22 21:52:15 +0900

    :bulb: #37 Add comments

--- src/controllers/drafts/DraftsUpdateServlet.java
+++ src/controllers/drafts/DraftsUpdateServlet.java
@@ -26,29 +26,33 @@ public class DraftsUpdateServlet extends HttpServlet {
 
         EntityManager em = DBUtil.createEntityManager();
 
+        // ログイン中のメンバーIDを取得
         Member loginMember = (Member) request.getSession().getAttribute("loginMember");
 
+        // 提出する下書きのアイデアIDを取得
         Idea idea = em.find(Idea.class, Integer.parseInt(request.getParameter("ideaId")));
 
+        // Ideaテーブルのレビュー状態を更新
         idea.setReview_flag(Integer.parseInt(request.getParameter("review_flag")));
 
         em.getTransaction().begin();
         em.getTransaction().commit();
         em.close();
 
+        // 提出する下書きのタイトルをString型で取得
         String ideaTitle = idea.getTitle();
 
         switch (loginMember.getRole_flag()) {
-        case 0:
+        case 0: // associateの場合
             request.getSession().setAttribute("toast", "日報「" + ideaTitle + "」を課長に提出しました。");
             break;
-        case 1:
+        case 1: // administratorの場合
             request.getSession().setAttribute("toast", "日報「" + ideaTitle + "」を課長に提出しました。");
             break;
-        case 2:
+        case 2: // managerの場合
             request.getSession().setAttribute("toast", "日報「" + ideaTitle + "」を部長に提出しました。");
             break;
-        case 3:
+        case 3: // directorの場合
             request.getSession().setAttribute("toast", "日報「" + ideaTitle + "」を提出しました。");
             break;
         }
