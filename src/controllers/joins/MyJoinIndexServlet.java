655bbab2f8043a0eda25efae2c268c6dd1f5bd9e
keiten.kiki <kikikeiten@gmail.com>
2020-11-22 14:46:32 +0900

    :fire: #37 Delete unnecessary comments

--- src/controllers/joins/MyJoinIndexServlet.java
+++ src/controllers/joins/MyJoinIndexServlet.java
@@ -15,27 +15,16 @@ import models.Join;
 import models.Member;
 import utils.DBUtil;
 
-/**
- * Servlet implementation class MyAttendanceIndexServlet
- */
 @WebServlet("/attendance/my")
 public class MyJoinIndexServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public MyJoinIndexServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        // TODO Auto-generated method stub
 
         EntityManager em = DBUtil.createEntityManager();
 
