95d2447f789a69fbe129257c05e7442425bb5a21
keiten.kiki <kikikeiten@gmail.com>
2020-11-22 14:40:27 +0900

    :fire: #37 Delete unnecessary comments

--- src/controllers/joins/LeaveEditServlet.java
+++ src/controllers/joins/LeaveEditServlet.java
@@ -13,27 +13,16 @@ import javax.servlet.http.HttpServletResponse;
 import models.Join;
 import utils.DBUtil;
 
-/**
- * Servlet implementation class LeaveIndexServlet
- */
-@WebServlet(name = "LeaveEditServlet", urlPatterns = { "/leave/edit" })
+@WebServlet(name = "LeaveEditServlet", urlPatterns = {"/leave/edit"})
 public class LeaveEditServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public LeaveEditServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        // TODO Auto-generated method stub
 
         EntityManager em = DBUtil.createEntityManager();
 
