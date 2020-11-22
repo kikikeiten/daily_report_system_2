5b4523e50a7cf8afecaea1b0a6ed57487eff2899
keiten.kiki <kikikeiten@gmail.com>
2020-11-22 20:14:23 +0900

    :recycle: #38 Fix DirectorAdviceCreateServlet

--- src/controllers/reviews/DirectorAdviceCreateServlet.java
+++ src/controllers/reviews/DirectorAdviceCreateServlet.java
@@ -25,17 +25,17 @@ public class DirectorAdviceCreateServlet extends HttpServlet {
 
         EntityManager em = DBUtil.createEntityManager();
 
-        Idea r = em.find(Idea.class, Integer.parseInt(request.getParameter("report_id")));
+        Idea idea = em.find(Idea.class, Integer.parseInt(request.getParameter("idea_id")));
 
-        r.setApproval(Integer.parseInt(request.getParameter("submit")));
+        idea.setReview_flag(Integer.parseInt(request.getParameter("reviewFlag")));
 
         em.getTransaction().begin();
         em.getTransaction().commit();
         em.close();
 
-        request.getSession().setAttribute("flush", "日報「" + r.getTitle() + "」を部長に再提出しました。");
+        request.getSession().setAttribute("toast", "日報「" + idea.getTitle() + "」を部長に再提出しました。");
 
-        response.sendRedirect(request.getContextPath() + "/remand/director");
+        response.sendRedirect(request.getContextPath() + "/advice/director");
     }
 
 }
