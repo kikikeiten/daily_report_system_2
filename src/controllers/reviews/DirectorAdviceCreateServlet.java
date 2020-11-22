f346f9960118816251fb4eab624a6a477f11e10a
keiten.kiki <kikikeiten@gmail.com>
2020-11-22 20:12:36 +0900

    :fire: #37 Delete unnecessary comments

--- src/controllers/reviews/DirectorAdviceCreateServlet.java
+++ src/controllers/reviews/DirectorAdviceCreateServlet.java
@@ -12,27 +12,16 @@ import javax.servlet.http.HttpServletResponse;
 import models.Idea;
 import utils.DBUtil;
 
-/**
- * Servlet implementation class DirectorRemandCreateServlet
- */
 @WebServlet("/director/remand/create")
 public class DirectorAdviceCreateServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public DirectorAdviceCreateServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-     */
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        // TODO Auto-generated method stub
 
         EntityManager em = DBUtil.createEntityManager();
 
