55717a2688266044c69bb73ceebbeb99347da806
keiten.kiki <kikikeiten@gmail.com>
2020-11-22 20:30:29 +0900

    :fire: #37 Delete unnecessary comments

--- src/controllers/ideas/DraftIndexServlet.java
+++ src/controllers/ideas/DraftIndexServlet.java
@@ -1,4 +1,4 @@
-package controllers.reviews;
+package controllers.ideas;
 
 import java.io.IOException;
 import java.util.List;
@@ -15,27 +15,16 @@ import models.Member;
 import models.Idea;
 import utils.DBUtil;
 
-/**
- * Servlet implementation class DraftIndexServlet
- */
 @WebServlet("/drafts")
 public class DraftIndexServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public DraftIndexServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        // TODO Auto-generated method stub
 
         EntityManager em = DBUtil.createEntityManager();
 
