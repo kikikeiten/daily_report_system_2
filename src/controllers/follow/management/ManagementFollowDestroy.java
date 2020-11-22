3e6159070970b8dadfe970d8eb9c8477daf55434
keiten.kiki <kikikeiten@gmail.com>
2020-11-22 10:05:47 +0900

    :bulb: #37 Add comments

--- src/controllers/follow/management/ManagementFollowDestroy.java
+++ src/controllers/follow/management/ManagementFollowDestroy.java
@@ -26,14 +26,18 @@ public class ManagementFollowDestroy extends HttpServlet {
 
         EntityManager em = DBUtil.createEntityManager();
 
+        // フォロー解除される側のメンバーIDを取得
         Follow follow = em.find(Follow.class, Integer.parseInt(request.getParameter("followed_id")));
 
+        // フォロー解除される側の氏名を取得
         Member member_id = follow.getMember();
         String member_name_str = member_id.getName();
 
+        // フォロー解除する側の氏名を取得
         Member unfollow_id = follow.getFollow();
         String unfollow_name_str = unfollow_id.getName();
 
+        // フォロー解除する側のメンバーIDを取得
         Member following_id = follow.getMember();
         Integer member_id_int = following_id.getId();
 
@@ -42,6 +46,7 @@ public class ManagementFollowDestroy extends HttpServlet {
         em.getTransaction().commit();
         em.close();
 
+        // トーストメッセージをセッションにセット
         request.getSession().setAttribute("toast", member_name_str + "さんが" + unfollow_name_str + "さんのフォローを解除しました。");
 
         response.sendRedirect(request.getContextPath() + "/management/unfollow?id=" + member_id_int);
