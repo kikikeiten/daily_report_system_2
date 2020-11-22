26cbf5f1d071629260d7e075f1987d555b9553b5
keiten.kiki <kikikeiten@gmail.com>
2020-11-22 20:22:10 +0900

    :fire: #37 Delete unnecessary comments

--- src/controllers/reviews/DirectorAdviceIndexServlet.java
+++ src/controllers/reviews/DirectorAdviceIndexServlet.java
@@ -15,27 +15,16 @@ import models.Member;
 import models.Idea;
 import utils.DBUtil;
 
-/**
- * Servlet implementation class DirectorRemandIndexServlet
- */
 @WebServlet("/remand/director")
 public class DirectorAdviceIndexServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public DirectorAdviceIndexServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        // TODO Auto-generated method stub
 
         EntityManager em = DBUtil.createEntityManager();
 
