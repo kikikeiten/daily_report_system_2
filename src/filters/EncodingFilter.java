d7b268fe7af66e1eb01b778c06e3a58ef250e7ae
keiten.kiki <kikikeiten@gmail.com>
2020-11-23 12:08:57 +0900

    :recycle: #39 Fix EncodingFilter

--- src/filters/EncodingFilter.java
+++ src/filters/EncodingFilter.java
@@ -21,6 +21,7 @@ public class EncodingFilter implements Filter {
 
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
+
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
 
