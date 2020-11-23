3cb9cc08ea68f4dc2af08f6f8af190e016aa515b
keiten.kiki <kikikeiten@gmail.com>
2020-11-23 12:23:33 +0900

    :recycle: #39 Fix TopPageIndexServlet

--- src/controllers/toppage/TopPageIndexServlet.java
+++ src/controllers/toppage/TopPageIndexServlet.java
@@ -30,8 +30,8 @@ public class TopPageIndexServlet extends HttpServlet {
 
         EntityManager em = DBUtil.createEntityManager();
 
-        // ログイン中メンバーのidを取得
-        Member login_member = (Member) request.getSession().getAttribute("login_member");
+        // ログイン中メンバーのIDを取得
+        Member loginMember = (Member) request.getSession().getAttribute("loginMember");
 
         // ページネーション
         int page;
@@ -41,69 +41,69 @@ public class TopPageIndexServlet extends HttpServlet {
             page = 1;
         }
 
-        // ログイン中メンバーのideaを取得
+        // ログイン中メンバーのアイデアを取得
         List<Idea> getMyIdeas = em.createNamedQuery("getMyIdeas", Idea.class)
-                .setParameter("login_member", login_member)
+                .setParameter("loginMember", loginMember)
                 .setFirstResult(12 * (page - 1))
                 .setMaxResults(12)
                 .getResultList();
 
-        // ログイン中メンバーのidea総数を取得
+        // ログイン中メンバーのアイデア総数を取得
         long getMyIdeasCnt = (long) em.createNamedQuery("getMyIdeasCnt", Long.class)
-                .setParameter("login_member", login_member)
+                .setParameter("loginMember", loginMember)
                 .getSingleResult();
 
-        // ログイン中メンバーの下書き総数を取得
+        // ログイン中メンバーのドラフト総数を取得
         long getMyDraftsCnt = (long) em.createNamedQuery("getMyDraftsCnt", Long.class)
-                .setParameter("login_member", login_member)
+                .setParameter("loginMember", loginMember)
                 .getSingleResult();
 
-        // マネージャーのアドバイス有idea総数を取得
+        // マネージャーのアドバイス有アイデア総数を取得
         long getManagerAdviceCnt = (long) em.createNamedQuery("getManagerAdviceCnt", Long.class)
-                .setParameter("login_member", login_member)
+                .setParameter("loginMember", loginMember)
                 .getSingleResult();
 
-        // ディレクターのアドバイス有idea総数を取得
+        // ディレクターのアドバイス有アイデア総数を取得
         long getDirectorAdviceCnt = (long) em.createNamedQuery("getDirectorAdviceCnt", Long.class)
-                .setParameter("login_member", login_member)
+                .setParameter("loginMember", loginMember)
                 .getSingleResult();
 
         // 今日の日付を取得
         Date today = new Date(System.currentTimeMillis());
 
-        // 下書きのポスト忘れ総数を取得（前日以前）
+        // ドラフトのポスト忘れ総数を取得（前日以前）
         long get4getMyDraftsCnt = (long) em.createNamedQuery("get4getMyDraftsCnt", Long.class)
                 .setParameter("today", today)
-                .setParameter("login_member", login_member)
+                .setParameter("loginMember", loginMember)
                 .getSingleResult();
 
-        // マネージャーのレビュー忘れideaを取得（前日以前）
+        // マネージャーのレビュー忘れアイデアを取得（前日以前）
         long get4getManagerReviewsCnt = (long) em
                 .createNamedQuery("get4getManagerReviewsCnt", Long.class)
                 .setParameter("today", today)
-                .setParameter("role_flag", login_member.getRole_flag())
+                .setParameter("role", loginMember.getRole())
                 .getSingleResult();
 
-        // ディレクターのレビュー忘れideaを取得（前日以前）
+        // ディレクターのレビュー忘れアイデアを取得（前日以前）
         long get4getDirectorReviewsCnt = (long) em
                 .createNamedQuery("get4getDirectorReviewsCnt", Long.class)
                 .setParameter("today", today)
-                .setParameter("role_flag", login_member.getRole_flag())
+                .setParameter("role", loginMember.getRole())
                 .getSingleResult();
 
-        // 下書きを除いたidea総数を取得
+        // ドラフトを除いたアイデア総数を取得
         long getIdeasCntButDrafts = (long) em.createNamedQuery("getIdeasCntButDrafts", Long.class)
                 .getSingleResult();
 
         try {
-            // punch_out忘れを取得（履歴がない場合もあるのでtry-catch）
+            // 退席忘れを取得（履歴がない場合もあるのでtry-catch）
             List<Join> get4getJoins = em.createNamedQuery("get4getJoins", Join.class)
                     .setParameter("today", today)
                     .getResultList();
 
-            // punch_out忘れのjoin_flagにそれぞれ2を付与
-            for (Join set_forget : get4getJoins) {
-                set_forget.setJoin_flag(2);
+            // 退席忘れのjoinStatusにそれぞれ2を付与
+            for (Join setForget : get4getJoins) {
+                setForget.setJoinStatus(2);
 
                 em.getTransaction().begin();
                 em.getTransaction().commit();
