082218ac5768da89bb968a31ea3c09bc661f03d1
keiten.kiki <kikikeiten@gmail.com>
2020-11-23 11:57:40 +0900

    :recycle: #39 Fix model of follow

--- src/models/Follow.java
+++ src/models/Follow.java
@@ -18,37 +18,37 @@ import lombok.Setter;
 
 @Table(name = "follows")
 @NamedQueries({
-        // フォロー解除するメンバーIDを取得
-        @NamedQuery(name = "getDestroyFollow", query = "SELECT f.id FROM Follow f WHERE f.followed_id  = :followed_id AND f.following_id = :login_member"),
+        // フォロー解除するメンバーのIDを取得
+        @NamedQuery(name = "getDestroyFollow", query = "SELECT f.id FROM Follow f WHERE f.followedId  = :followedId AND f.followingId = :loginMember"),
         // フォロー解除するフォロワーのIDを取得
-        @NamedQuery(name = "getDestroyFollower", query = "SELECT f.id FROM Follow f WHERE f.followed_id  = :login_member AND f.following_id = :following_id"),
+        @NamedQuery(name = "getDestroyFollower", query = "SELECT f.id FROM Follow f WHERE f.followedId  = :loginMember AND f.followingId = :followingId"),
 
-        // ログイン中メンバーがフォローしているメンバーのideaを取得
-        @NamedQuery(name = "getMyFollowingIdeas", query = "SELECT i FROM Idea i, Follow f WHERE f.following_id = :login_member AND i.member.id = f.followed_id.id ORDER BY i.updated_at DESC"),
-        // カウント
-        @NamedQuery(name = "getMyFollowingIdeasCnt", query = "SELECT COUNT(i) FROM Idea i, Follow f WHERE f.followed_id = :login_member AND i.member.id = f.followed_id.id"),
+        // ログイン中メンバーがフォローしているメンバーのアイデアを取得
+        @NamedQuery(name = "getMyFollowingIdeas", query = "SELECT i FROM Idea i, Follow f WHERE f.followingId = :loginMember AND i.member.id = f.followedId.id ORDER BY i.updatedAt DESC"),
+        // カウントを取得
+        @NamedQuery(name = "getMyFollowingIdeasCnt", query = "SELECT COUNT(i) FROM Idea i, Follow f WHERE f.followedId = :loginMember AND i.member.id = f.followedId.id"),
 
         // ログイン中メンバーのフォロー一覧を取得
-        @NamedQuery(name = "getMyFollowing", query = "SELECT f FROM Follow f, Member m WHERE f.following_id = :login_member AND m.id = f.followed_id.id ORDER BY f.updated_at DESC"),
-        // カウント
-        @NamedQuery(name = "getMyFollowingCnt", query = "SELECT COUNT(f) FROM Follow f, Member m WHERE f.following_id = :login_member AND m.id = f.followed_id.id"),
-        // ログイン中メンバーのフォローしているメンバーIDを取得（削除予定）
-        @NamedQuery(name = "checkMyFollow", query = "SELECT f.followed_id FROM Follow f WHERE f.following_id = :login_member"),
+        @NamedQuery(name = "getMyFollowing", query = "SELECT f FROM Follow f, Member m WHERE f.followingId = :loginMember AND m.id = f.followedId.id ORDER BY f.updatedAt DESC"),
+        // カウントを取得
+        @NamedQuery(name = "getMyFollowingCnt", query = "SELECT COUNT(f) FROM Follow f, Member m WHERE f.followingId = :loginMember AND m.id = f.followedId.id"),
+        // ログイン中メンバーがフォローしているメンバーのIDを取得（削除予定）
+        @NamedQuery(name = "checkMyFollow", query = "SELECT f.followedId FROM Follow f WHERE f.followingId = :loginMember"),
 
         // ログイン中メンバーのフォロワー一覧を取得
-        @NamedQuery(name = "getMyFollower", query = "SELECT f FROM Follow f, Member m WHERE f.followed_id = :login_member AND m.id = f.following_id.id ORDER BY f.updated_at DESC"),
-        // カウント
-        @NamedQuery(name = "getMyFollowerCnt", query = "SELECT COUNT(f) FROM  Follow f, Member m WHERE f.followed_id = :login_member AND m.id = f.following_id.id"),
+        @NamedQuery(name = "getMyFollower", query = "SELECT f FROM Follow f, Member m WHERE f.followedId = :loginMember AND m.id = f.followingId.id ORDER BY f.updatedAt DESC"),
+        // カウントを取得
+        @NamedQuery(name = "getMyFollowerCnt", query = "SELECT COUNT(f) FROM  Follow f, Member m WHERE f.followedId = :loginMember AND m.id = f.followingId.id"),
 
-        // 対象メンバーのフォロー一覧を表示
-        @NamedQuery(name = "getMemberFollowing", query = "SELECT f FROM Follow f WHERE f.following_id = :login_member ORDER BY f.updated_at DESC"),
-        // カウント
-        @NamedQuery(name = "getMemberFollowingCnt", query = "SELECT COUNT(f) FROM Follow f WHERE f.following_id = :login_member"),
+        // 対象メンバーのフォロー一覧を取得
+        @NamedQuery(name = "getMemberFollowing", query = "SELECT f FROM Follow f WHERE f.followingId = :loginMember ORDER BY f.updatedAt DESC"),
+        // カウントを取得
+        @NamedQuery(name = "getMemberFollowingCnt", query = "SELECT COUNT(f) FROM Follow f WHERE f.followingId = :loginMember"),
 
-        // 対象のメンバーがフォローしていないメンバー一覧を表示
-        @NamedQuery(name = "getMemberNotFollowing", query = "SELECT m FROM Member m WHERE NOT EXISTS (SELECT f FROM Follow f WHERE f.following_id = :login_member AND m.id = f.followed_id.id) AND m.id <> :login_member ORDER BY m.updated_at DESC"),
-        // カウント
-        @NamedQuery(name = "getMemberNotFollowingCnt", query = "SELECT COUNT(m) FROM Member m WHERE NOT EXISTS (SELECT f FROM Follow f WHERE f.following_id = :login_member AND m.id = f.followed_id.id) AND m.id <> :login_member")
+        // 対象のメンバーがフォローしていないメンバー一覧を取得
+        @NamedQuery(name = "getMemberNotFollowing", query = "SELECT m FROM Member m WHERE NOT EXISTS (SELECT f FROM Follow f WHERE f.followingId = :loginMember AND m.id = f.followedId.id) AND m.id <> :loginMember ORDER BY m.updated_at DESC"),
+        // カウントを取得
+        @NamedQuery(name = "getMemberNotFollowingCnt", query = "SELECT COUNT(m) FROM Member m WHERE NOT EXISTS (SELECT f FROM Follow f WHERE f.followingId = :loginMember AND m.id = f.followedId.id) AND m.id <> :loginMember")
 })
 
 @Getter
@@ -62,17 +62,17 @@ public class Follow {
 
     // フォローする側のメンバーid
     @ManyToOne
-    @JoinColumn(name = "following_id", nullable = false)
-    private Member following_id;
+    @JoinColumn(name = "followingId", nullable = false)
+    private Member followingId;
 
     // フォローされる側のメンバーid
     @ManyToOne
-    @JoinColumn(name = "followed_id", nullable = false)
-    private Member followed_id;
+    @JoinColumn(name = "followedId", nullable = false)
+    private Member followedId;
 
-    @Column(name = "created_at", nullable = false)
-    private Timestamp created_at;
+    @Column(name = "createdAt", nullable = false)
+    private Timestamp createdAt;
 
-    @Column(name = "updated_at", nullable = false)
-    private Timestamp updated_at;
+    @Column(name = "updatedAt", nullable = false)
+    private Timestamp updatedAt;
 }
