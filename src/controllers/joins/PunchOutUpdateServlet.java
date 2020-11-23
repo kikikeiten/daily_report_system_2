dc341f7ab3cfbe17226453e50c74b8e99c22418d
keiten.kiki <kikikeiten@gmail.com>
2020-11-23 15:33:57 +0900

    :recycle: #39 Fix PunchOutUpdateServlet

--- src/controllers/joins/PunchOutUpdateServlet.java
+++ src/controllers/joins/PunchOutUpdateServlet.java
@@ -30,41 +30,41 @@ public class PunchOutUpdateServlet extends HttpServlet {
         EntityManager em = DBUtil.createEntityManager();
 
         // 退席したメンバーのIDを取得
-        Join join = em.find(Join.class, Integer.parseInt(request.getParameter("join_id")));
+        Join join = em.find(Join.class, Integer.parseInt(request.getParameter("joinId")));
 
         // 入力された時刻をString型で保存
-        String punch_out_time_str = Integer.parseInt(request.getParameter("punch_out_hour")) + ":"
-                + Integer.parseInt(request.getParameter("punch_out_minute")) + ":00";
+        String punchOutTimeStr = Integer.parseInt(request.getParameter("punchOutHour")) + ":"
+                + Integer.parseInt(request.getParameter("punchOutMinute")) + ":00";
 
         // Time型に変換
-        Time punch_out_time = Time.valueOf(punch_out_time_str);
+        Time punchOutTime = Time.valueOf(punchOutTimeStr);
         // 詳細な現在時刻を取得
-        Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
 
         // Joinテーブルに値をセット
-        join.setPunch_out(punch_out_time);
-        join.setJoin_flag(3); // 3 == 退席時間の修正済み
-        join.setCreated_at(currentTime);
-        join.setUpdated_at(currentTime);
+        join.setPunchOut(punchOutTime);
+        join.setJoinStatus(3); // 3 == 退席時間の修正済み
+        join.setCreatedAt(timestamp);
+        join.setUpdatedAt(timestamp);
 
         // 参加時刻をLocalTime型に変換
-        LocalTime punch_in_local = join.getPunch_in().toLocalTime();
+        LocalTime punchInLocal = join.getPunchIn().toLocalTime();
         // LocalTime型の現在時刻を取得
-        LocalTime punch_out_local = punch_out_time.toLocalTime();
-        // 現在時刻と参加時間との差分を求める（working）
-        long minutes = ChronoUnit.MINUTES.between(punch_in_local, punch_out_local);
+        LocalTime punchOutLocal = punchOutTime.toLocalTime();
+        // 現在時刻と参加時間との差分を求める
+        long minutes = ChronoUnit.MINUTES.between(punchInLocal, punchOutLocal);
 
         // 時間と分を割り出す
-        long diff_hours = minutes / 60;
-        long diff_minutes = minutes % 60;
+        long diffHours = minutes / 60;
+        long diffMinutes = minutes % 60;
 
         // HH:mmの形でString型に保存
-        String diff_time_str = diff_hours + ":" + diff_minutes + ":00";
+        String diffTime = diffHours + ":" + diffMinutes + ":00";
         // Time型に変換
-        Time working_time = Time.valueOf(diff_time_str);
+        Time workingTime = Time.valueOf(diffTime);
 
         // Joinテーブルに値をセット
-        join.setWorking(working_time);
+        join.setWorkingtime(workingTime);
 
         em.getTransaction().begin();
         em.getTransaction().commit();
