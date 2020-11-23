536b1a41b15f6d8c1e4f558f0b9645af4615b9a8
keiten.kiki <kikikeiten@gmail.com>
2020-11-23 14:07:24 +0900

    :recycle: #39 Fix JoinsIndexServlet

--- src/controllers/joins/JoinsIndexServlet.java
+++ src/controllers/joins/JoinsIndexServlet.java
@@ -35,7 +35,7 @@ public class JoinsIndexServlet extends HttpServlet {
             page = 1;
         }
 
-        // 全てのjoin履歴を取得
+        // 全てのジョイン履歴を取得
         List<Join> getJoins = em.createNamedQuery("getJoins", Join.class)
                 .setFirstResult(12 * (page - 1))
                 .setMaxResults(12)
