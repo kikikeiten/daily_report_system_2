cdf424f1360467b0649c78d2505e238d4cce1670
keiten.kiki <kikikeiten@gmail.com>
2020-11-27 10:36:05 +0900

    :recycle: #48 Fix ReviewsDirectorCreate

--- src/controllers/reviews/director/ReviewsDirectorCreateServlet.java
+++ src/controllers/reviews/director/ReviewsDirectorCreateServlet.java
@@ -10,9 +10,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import models.Review;
-import models.Member;
 import models.Idea;
+import models.Member;
+import models.Review;
 import utils.DBUtil;
 
 @WebServlet("/reviews/director/create")
@@ -56,50 +56,43 @@ public class ReviewsDirectorCreateServlet extends HttpServlet {
         em.getTransaction().commit();
         em.close();
 
-        // レビューされるメンバーIDを取得
-        Member ideaMember = idea.getMember();
-        // メンバーの役割を取得
-        Integer role = ideaMember.getRole();
-        // レビューされるメンバー名を取得
-        String ideaMemberName = ideaMember.getName();
-
         // 値に応じてトーストメッセージをセッションにセット
         if (reviewStatus == 3) { // アイデアにアドバイスを付けて差し戻す
-            switch (role) {
+            switch (idea.getMember().getRole()) {
                 case 0: // アソシエイト宛て
                     request.getSession().setAttribute("toast",
-                            "日報「" + idea.getTitle() + "」を" + ideaMemberName + "社員に差し戻しました。");
+                            "日報「" + idea.getTitle() + "」を" + idea.getMember().getName() + "社員に差し戻しました。");
                     break;
                 case 1: // 管理者宛て
                     request.getSession().setAttribute("toast",
-                            "日報「" + idea.getTitle() + "」を" + ideaMemberName + "管理者に差し戻しました。");
+                            "日報「" + idea.getTitle() + "」を" + idea.getMember().getName() + "管理者に差し戻しました。");
                     break;
                 case 2: // マネージャー宛て
                     request.getSession().setAttribute("toast",
-                            "日報「" + idea.getTitle() + "」を" + ideaMemberName + "課長に差し戻しました。");
+                            "日報「" + idea.getTitle() + "」を" + idea.getMember().getName() + "課長に差し戻しました。");
                     break;
                 case 3: // ディレクター宛て
                     request.getSession().setAttribute("toast",
-                            "日報「" + idea.getTitle() + "」を" + ideaMemberName + "部長に差し戻しました。");
+                            "日報「" + idea.getTitle() + "」を" + idea.getMember().getName() + "部長に差し戻しました。");
                     break;
             }
         } else { // アイデアを承認する
-            switch (role) {
+            switch (idea.getMember().getRole()) {
                 case 0: // アソシエイトから
                     request.getSession().setAttribute("toast",
-                            ideaMemberName + "社員の日報「" + idea.getTitle() + "」を承認しました。");
+                            idea.getMember().getName() + "社員の日報「" + idea.getTitle() + "」を承認しました。");
                     break;
                 case 1: // 管理者から
                     request.getSession().setAttribute("toast",
-                            ideaMemberName + "管理者の日報「" + idea.getTitle() + "」を承認しました。");
+                            idea.getMember().getName() + "管理者の日報「" + idea.getTitle() + "」を承認しました。");
                     break;
                 case 2: // マネージャーから
                     request.getSession().setAttribute("toast",
-                            ideaMemberName + "課長の日報「" + idea.getTitle() + "」を承認しました。");
+                            idea.getMember().getName() + "課長の日報「" + idea.getTitle() + "」を承認しました。");
                     break;
                 case 3: // ディレクターから
                     request.getSession().setAttribute("toast",
-                            ideaMemberName + "部長の日報「" + idea.getTitle() + "」を承認しました。");
+                            idea.getMember().getName() + "部長の日報「" + idea.getTitle() + "」を承認しました。");
                     break;
             }
         }
